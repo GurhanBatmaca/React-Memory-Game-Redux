f450fe22e4341aaf9bdff66d22fa39dc4389bc0f
GurhanBatmaca <gurhanbtmc@hotmail.com>
2023-02-22 19:49:55 +0300

    mix to array,added some styles

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,22 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { clickCard } from "../redux/slices/cardsSlice";
 
 export const CardList = () => {
     const cardList = useSelector(state => state.cards.cardList);
+
+    const dispatch = useDispatch();
+
   return (
     <div className='p-5 row card-list'>
         {
             cardList.map((card,index) => (
-                <div key={index} className="col-sm-2 col-6">
-                    <div className="p-2">
-                        <div className="my-card">
+                <div onClick={() => {dispatch(clickCard(index))}} key={index} className="col-sm-2 col-6">
+                    <div className="p-2 card-top">
+                        <div className={ (card.status ? "card-wall" : "card-head") +" "}>
                             <img src={card.img} />
                             <h5>{card.name}</h5>
                         </div>
-                        <div>
+                        <div className={ (card.status ? "card-head" : "card-wall") + " "}>
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"/>
                         </div>
                     </div>
--- src/redux/slices/cardsSlice.js
+++ src/redux/slices/cardsSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";import { data } from "../../data/data";
+const fullCardList = [...data,...data].sort(function(){
+    return 0.5 - Math.random()
+})
+// fullCardList.sort(function(){
+//     return 0.5 - Math.random()
+// })
 
 const cardsSlice = createSlice({
     name: "cards",
     initialState: {
-        cardList: data
+        cardList: fullCardList
     },
-    reducers: {}
+    reducers: {
+        clickCard: (state,action) => {
+            state.cardList.map((card,index) => {
+                if(index === action.payload) {
+                    card.status = true;
+                    console.log(card.name);
+                    console.log(card.status);
+                }
+            })
+        }
+    }
 })
+
+export const { clickCard } = cardsSlice.actions;
 
 export default cardsSlice.reducer;
