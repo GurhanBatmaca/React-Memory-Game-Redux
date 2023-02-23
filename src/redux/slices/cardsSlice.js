8140f117b3fc0f10ca64f1ec39e5d8c225d48a8a
GurhanBatmaca <gurhanbtmc@hotmail.com>
2023-02-23 19:34:44 +0300

    error hendler

--- src/redux/slices/cardsSlice.js
+++ src/redux/slices/cardsSlice.js
@@ -1,26 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";import { data } from "../../data/data";
 const fullCardList = [...data,...data].sort(function(){
-    return 0.5 - Math.random()
-})
-// fullCardList.sort(function(){
-//     return 0.5 - Math.random()
-// })
+    return 0.5 - Math.random();
+});
 
 const cardsSlice = createSlice({
     name: "cards",
     initialState: {
-        cardList: fullCardList
+        cardList: fullCardList,
+        clickedCards : [],
+        clicedItem : {},
+        clickIndex: 0
     },
     reducers: {
         clickCard: (state,action) => {
             state.cardList.map((card,index) => {
                 if(index === action.payload) {
-                    card.status = true;
-                    console.log(card.name);
-                    console.log(card.status);
+                    state.clickIndex += 1;
+                    if(state.clickIndex == 1) {                       
+                        card.status = true;
+                        state.clicedItem = card;    
+                    }
+                    if(state.clickIndex == 2) {
+                        card.status = true;
+                        if(card.name === state.clicedItem.name) {
+                            state.clickedCards.push(card);
+                            state.clickedCards.push(state.clicedItem);
+                            state.clicedItem = {};
+                            state.clickIndex = 0
+                        } else {
+                            card.status = false;
+                            state.clicedItem.status == false
+                            state.clicedItem = {};
+                            state.clickIndex = 0
+                        }
+                                              
+                    }                  
+                    
                 }
             })
+            console.log(fullCardList); 
+            console.log(state.cardList)
         }
+        
     }
 })
 
